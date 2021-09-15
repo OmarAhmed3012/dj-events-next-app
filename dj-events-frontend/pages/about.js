31ccf51552d3c9faf874e9aa553c2ab0964be39e
OmarAhmed3012 <omaraboshamaa@gmail.com>
2021-09-15 17:26:14 +0200

    Layout, Head Tags, Global CSS

---
+++ dj-events-frontend/pages/about.js
@@ -0,0 +1,10 @@
+import Layout from "./../components/Layout";
+
+export default function AboutPage() {
+  return (
+    <Layout title="About DJ Events">
+      <h1>About</h1>
+      <a href="/">Home</a>
+    </Layout>
+  );
+}
