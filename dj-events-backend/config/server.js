6732a1aa73f0b486616f04351f59bd1c7cd8417b
OmarAhmed3012 <omaraboshamaa@gmail.com>
2021-10-23 19:08:50 +0200

    Strapi backend & Cloudinary config

---
+++ dj-events-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '344ca64299ae4120590b470a11bde443'),
+    },
+  },
+});
---
+++ dj-events-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '136b0b3a-fb93-4e7b-a05d-cc524dc3f854'
+};
