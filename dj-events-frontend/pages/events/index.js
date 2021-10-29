19cfb90850599be478d286871038ba00516a13c2
OmarAhmed3012 <omaraboshamaa@gmail.com>
2021-10-29 02:00:03 +0200

    Events Pagination

--- dj-events-frontend/pages/events/index.js
+++ dj-events-frontend/pages/events/index.js
@@ -1,8 +1,10 @@
 import Layout from "@/components/layout";
 import Eventitem from "@/components/EventItem";
 import { API_URL } from "./../../../config/index";
+import Pagination from "@/components/pagination";
+import { PER_PAGE } from "./../../../config/index";
 
-export default function EventsHome({ events }) {
+export default function EventsHome({ events, page, total }) {
   return (
     <Layout>
       <h1>Events</h1>
@@ -11,16 +13,24 @@ export default function EventsHome({ events }) {
       {events.map((evt) => (
         <Eventitem key={evt.id} evt={evt} />
       ))}
+
+      <Pagination page={page} total={total} />
     </Layout>
   );
 }
 
-export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/events`);
-  const events = await res.json();
+export async function getServerSideProps({ query: { page = 1 } }) {
+  const start = +page === 1 ? 0 : (+page - 1) * PER_PAGE;
+
+  const totalRes = await fetch(`${API_URL}/events/count`);
+  const total = await totalRes.json();
+
+  const eventRes = await fetch(
+    `${API_URL}/events?_sort=date:ASC&_limit=${PER_PAGE}&_start=${start}`
+  );
+  const events = await eventRes.json();
 
   return {
-    props: { events },
-    revalidate: 1,
+    props: { events, page: +page, total },
   };
 }
